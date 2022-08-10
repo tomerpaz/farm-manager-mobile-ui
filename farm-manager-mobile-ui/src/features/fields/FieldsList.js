dd2fc80c55883ce4f9c490f962d32e1945adae22
tomerpaz <paz.tomer@gmail.com>
2022-08-10 18:44:47 +0300

    fix

---
+++ farm-manager-mobile-ui/src/features/fields/FieldsList.js
@@ -0,0 +1,10 @@
+import { useSelector } from "react-redux";import React from 'react'
+import React from 'react'
+
+const FieldsList = () => {
+  return (
+    <div>FieldsList</div>
+  )
+}
+
+export default FieldsList
