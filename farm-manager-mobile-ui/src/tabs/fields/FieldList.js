bd76b7b52177db42c0acba46232df9b2522fba85
tomerpaz <paz.tomer@gmail.com>
2022-08-17 09:53:23 +0300

    fix

--- farm-manager-mobile-ui/src/tabs/fields/FieldList.js
+++ farm-manager-mobile-ui/src/tabs/fields/FieldList.js
@@ -4,7 +4,7 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
 import { FixedSizeList, } from 'react-window';
-import { Divider, ListItemAvatar, Avatar, BottomNavigation, BottomNavigationAction, IconButton } from '@mui/material';
+import { Divider, ListItemAvatar, Avatar, BottomNavigation, BottomNavigationAction, IconButton, Typography } from '@mui/material';
 import WorkIcon from '@mui/icons-material/Work';
 import List from '@mui/icons-material/List';
 import RestoreIcon from '@mui/icons-material/Restore';
@@ -13,6 +13,52 @@ import LocationOnIcon from '@mui/icons-material/LocationOn';
 import DeleteIcon from '@mui/icons-material/Delete';
 
 
+function renderRow(props) {
+    const { index, style, field } = props;
+    const cropEngName = field.cropEngName
+    return (
+        <Box style={style}>
+
+            <ListItem key={index} component="div" disablePadding
+            // secondaryAction={
+            //     <IconButton edge="end" aria-label="delete">
+            //         <DeleteIcon />
+            //     </IconButton>
+            // }
+            >
+                <ListItemButton>
+                    <ListItemAvatar>
+                        <Avatar>
+                            <WorkIcon />
+                        </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText primary={
+                        <Box display={'flex'} flexDirection={'row'} justifyContent={'space-between'}>
+                            <Typography>
+                                {`${field.name}`}
+                            </Typography>
+                            <Typography>
+                                {`${field.area}`}
+                            </Typography>
+                        </Box>
+                    }
+                        secondary={
+                            <Box component={"span"} display={'flex'} flexDirection={'row'} justifyContent={'space-between'}>
+                                <Typography component={"span"}>
+                                    {`${field.cropName},${field.varietyName}`}
+                                </Typography>
+                                <Typography component={"span"}>
+                                    {`${field.startDate}`}
+                                </Typography>
+                            </Box>
+                        }
+                    />
+                </ListItemButton>
+            </ListItem>
+            <Divider />
+        </Box>
+    );
+}
 
 
 export default function FieldList(props) {
@@ -23,34 +69,33 @@ export default function FieldList(props) {
     const { fields } = props;
 
 
-    const renderRow = (props) => {
-        const { index, style } = props;
-    console.log(fields[index]);
-        return (
-            <Box>
-                <ListItem key={index} component="div" disablePadding
-    
-                    secondaryAction={
-                        <IconButton edge="end" aria-label="delete">
-                            <DeleteIcon />
-                        </IconButton>
-                    }
-                >
-                    <ListItemButton>
-                        <ListItemAvatar>
-                            <Avatar>
-                                <WorkIcon />
-                            </Avatar>
-                        </ListItemAvatar>
-                        <ListItemText primary={`${fields[index].name}, ${fields[index].cropName},${fields[index].varietyName}`} secondary={fields[index].startDate} />
-                    </ListItemButton>
-                </ListItem>
-                <Divider />
-    
-    
-            </Box>
-        );
-    }
+    // const renderRow = (props) => {
+    //     const { index, style } = props;
+    //     return (
+    //         <Box>
+    //             <ListItem key={index} component="div" disablePadding
+
+    //                 secondaryAction={
+    //                     <IconButton edge="end" aria-label="delete">
+    //                         <DeleteIcon />
+    //                     </IconButton>
+    //                 }
+    //             >
+    //                 <ListItemButton>
+    //                     <ListItemAvatar>
+    //                         <Avatar>
+    //                             <WorkIcon />
+    //                         </Avatar>
+    //                     </ListItemAvatar>
+    //                     <ListItemText primary={`${fields[index].name}, ${fields[index].cropName},${fields[index].varietyName}`} secondary={fields[index].startDate} />
+    //                 </ListItemButton>
+    //             </ListItem>
+    //             <Divider />
+
+
+    //         </Box>
+    //     );
+    // }
 
     return (
         <Box
@@ -59,11 +104,12 @@ export default function FieldList(props) {
             <FixedSizeList
                 height={height}
 
-                itemSize={46}
+                itemSize={70}
                 itemCount={fields.length}
                 overscanCount={5}
             >
-                {renderRow}
+                {(props) => renderRow({ ...props, field: fields[props.index] })}
+
             </FixedSizeList>
             <BottomNavigation
                 showLabels
